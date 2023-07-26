9a1aa5c99196f1ddcd91595aae62f76063344b12
Manh Nguyen <manhnd9293@gmail.com>
2023-07-26 11:28:23 +0700

    add auth sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,34 @@
-import { Injectable, Post } from '@nestjs/common';
+import { ForbiddenException, Injectable, Post } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './auth.dto';
+import { hash } from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
-  signup() {
-    return { data: 'Sign up success' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      const hashPassword = await hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hashPassword,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Credential taken');
+        }
+      }
+    }
   }
 
   signin() {
